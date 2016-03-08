bc604da64c2253033000956a4f38b0ace2756e3b
sanyinchen <1554194922@qq.com>
2016-03-08 23:53:34 +0800

    dev

--- src/main/groovy/com/tony/dir/MainPortal.groovy
+++ src/main/groovy/com/tony/dir/MainPortal.groovy
@@ -1,7 +1,15 @@
-package main.groovy.com.tony.dir
+package com.tony.dir
+
+
+import org.gradle.api.*;
 
 /**
  * Created by sanyinchen on 16/3/3.
  */
-class MainPortal  {
+class MainPortal implements Plugin<Project> {
+    def void apply(Project project) {
+        project.task('hello')<<{
+            println 'Hello dev'
+        }
+    }
 }
